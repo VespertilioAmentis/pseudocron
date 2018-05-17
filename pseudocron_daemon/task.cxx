6f627f4407e4b90d5fbd99a61cc99d36780669b7
Toniy <vespertilioamentis@yandex.ru>
2018-05-18 00:27:27 +0300

    3 out of four initial tests.

--- pseudocron_daemon/task.cxx
+++ pseudocron_daemon/task.cxx
@@ -1,19 +1,52 @@
 #include "task.hxx"
 #include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
+#include <vector>
+
+enum task_fields
+{
+    e_min,
+    e_hr,
+    e_dy,
+    //
+    e_cnt
+};
+
+using str_vec = std::vector<std::string>;
+
+static str_vec splitstr(const std::string& c_str)
+{
+    str_vec v;
+    boost::split(v, c_str, boost::is_any_of(" "));
+    return v;
+}
+
+template<task_fields _val>
+static unsigned extractNumeric(const std::string& c_strTask)
+{
+    const auto c_v = splitstr(c_strTask);
+    try
+    {
+    return boost::lexical_cast<unsigned>(c_v.at(_val));
+    }
+    catch(const boost::bad_lexical_cast&)
+    {}
+    return 0;
+}
 
 unsigned extractMinutesVal(const std::string &c_strTask)
 {
-    return 0;
+    return extractNumeric<e_min>(c_strTask);
 }
 
 unsigned extractHoursVal(const std::string &c_strTask)
 {
-    return 0;
+    return extractNumeric<e_hr>(c_strTask);
 }
 
 unsigned extractDaysVal(const std::string &c_strTask)
 {
-    return 0;
+    return extractNumeric<e_dy>(c_strTask);
 }
 
 std::string extractCmd(const std::string &c_strTask)
