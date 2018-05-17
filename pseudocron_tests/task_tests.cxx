733492d0f7732e182b7a2ad702da9ba1813a45dc
Toniy <vespertilioamentis@yandex.ru>
2018-05-18 00:46:28 +0300

    Auto.

--- pseudocron_tests/task_tests.cxx
+++ pseudocron_tests/task_tests.cxx
@@ -4,7 +4,7 @@
 // * * * task
 // */2 * * task
 
-static const std::string sc_strNoFracs = "5 14 3 echo blah";
+static const std::string sc_strFracs = "5/2 14/3 3/4 echo blah";
 static const unsigned sc_uMin = 5;
 static const unsigned sc_uHr = 14;
 static const unsigned sc_uDay = 3;
@@ -13,27 +13,27 @@ static const std::string sc_strCmd = "echo blah";
 TEST(task_tests,
      minutes_value_is_extracted_from_string)
 {
-    const unsigned c_uMin = extractMinutesVal(sc_strNoFracs);
+    const auto c_uMin = extractMinutesVal(sc_strFracs);
     ASSERT_EQ(sc_uMin, c_uMin);
 }
 
 TEST(task_tests,
      hours_value_is_extracted_from_string)
 {
-    const unsigned c_uHr = extractHoursVal(sc_strNoFracs);
+    const auto c_uHr = extractHoursVal(sc_strFracs);
     ASSERT_EQ(sc_uHr, c_uHr);
 }
 
 TEST(task_tests,
      days_value_is_extracted_from_string)
 {
-    const unsigned c_uDy = extractDaysVal(sc_strNoFracs);
+    const auto c_uDy = extractDaysVal(sc_strFracs);
     ASSERT_EQ(sc_uDay, c_uDy);
 }
 
 TEST(task_tests,
      cmd_is_extracted_from_string)
 {
-    const std::string strCmd = extractCmd(sc_strNoFracs);
+    const auto strCmd = extractCmd(sc_strFracs);
     ASSERT_EQ(sc_strCmd, strCmd);
 }
