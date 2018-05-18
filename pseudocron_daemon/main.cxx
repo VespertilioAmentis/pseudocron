31535b67e5ba5b629ba978fa4351033a38168ed3
Toniy <vespertilioamentis@yandex.ru>
2018-05-19 01:23:22 +0300

    The simplest possible main.

--- pseudocron_daemon/main.cxx
+++ pseudocron_daemon/main.cxx
@@ -1,9 +1,31 @@
-#include <iostream>
-
-using namespace std;
+#include "task.hxx"
+#include <fstream>
+#include <boost/foreach.hpp>
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    std::ifstream ff("/home/toniy/pseudocron.txt");
+    std::string str;
+    using tasks_vec  = std::vector<Task>;
+    tasks_vec vTasks;
+    do
+    {
+        std::getline(ff, str);
+        if(!str.empty())
+        {
+            Task task(str);
+            vTasks.push_back(task);
+        }
+    }
+    while(!str.empty());
+    while(true)
+    {
+        const boost::posix_time::ptime c_now = boost::posix_time::second_clock::local_time();
+        BOOST_FOREACH(tasks_vec::value_type& task, vTasks)
+        {
+            const bool c_bRes = task.checkTimeAndExec(c_now);
+            std::cout << (c_bRes ? "Executed" : "Skipped") << std::endl;
+        }
+    }
     return 0;
 }
