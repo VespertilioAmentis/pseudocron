2ca28c9b6302a0bbe31c5a4f49cbb3d5e9b1f8a1
Toniy <vespertilioamentis@yandex.ru>
2018-05-18 22:58:07 +0300

    Red tests for intrv extr from starless string.

--- pseudocron_tests/task_tests.cxx
+++ pseudocron_tests/task_tests.cxx
@@ -4,7 +4,7 @@
 // * * * task
 // */2 * * task
 
-static const std::string sc_strFracs = "5/8 14/9 3/10 echo blah";
+static const std::string sc_strNums = "5 14 3 echo blah";
 static const std::string sc_strStars = "*/8 */9 */10 echo blah";
 static const int sc_iMin = 5;
 static const int sc_iHr = 14;
@@ -17,77 +17,70 @@ static const std::string sc_strCmd = "echo blah";
 TEST(task_tests,
      minutes_value_is_extracted_from_string)
 {
-    const auto c_uMin = extractMinutesVal(sc_strFracs);
+    const auto c_uMin = extractMinutesVal(sc_strNums);
     ASSERT_EQ(sc_iMin, c_uMin);
 }
 
 TEST(task_tests,
      hours_value_is_extracted_from_string)
 {
-    const auto c_uHr = extractHoursVal(sc_strFracs);
+    const auto c_uHr = extractHoursVal(sc_strNums);
     ASSERT_EQ(sc_iHr, c_uHr);
 }
 
 TEST(task_tests,
      days_value_is_extracted_from_string)
 {
-    const auto c_uDy = extractDaysVal(sc_strFracs);
+    const auto c_uDy = extractDaysVal(sc_strNums);
     ASSERT_EQ(sc_iDay, c_uDy);
 }
 
 TEST(task_tests,
      minutes_interval_is_extracted_from_string)
 {
-    const auto c_uMinIntrv = extractMinutesIntrv(sc_strFracs);
+    const auto c_uMinIntrv = extractMinutesIntrv(sc_strStars);
     ASSERT_EQ(sc_iMinIntrv, c_uMinIntrv);
 }
 
 TEST(task_tests,
      hours_interval_is_extracted_from_string)
 {
-    const auto c_uHrIntrv = extractHoursIntrv(sc_strFracs);
+    const auto c_uHrIntrv = extractHoursIntrv(sc_strStars);
     ASSERT_EQ(sc_iHrIntrv, c_uHrIntrv);
 }
 
 TEST(task_tests,
      days_interval_is_extracted_from_string)
 {
-    const auto c_uDyIntrv = extractDaysIntrv(sc_strFracs);
-    ASSERT_EQ(sc_iDayIntrv, c_uDyIntrv);
-}
-
-TEST(task_tests,
-     minutes_interval_is_extracted_from_string_with_stars)
-{
-    const auto c_uMinIntrv = extractMinutesIntrv(sc_strStars);
-    ASSERT_EQ(sc_iMinIntrv, c_uMinIntrv);
-}
-
-TEST(task_tests,
-     hours_interval_is_extracted_from_string_with_stars)
-{
-    const auto c_uHrIntrv = extractHoursIntrv(sc_strStars);
-    ASSERT_EQ(sc_iHrIntrv, c_uHrIntrv);
-}
-
-TEST(task_tests,
-     days_interval_is_extracted_from_string_with_stars)
-{
     const auto c_uDyIntrv = extractDaysIntrv(sc_strStars);
     ASSERT_EQ(sc_iDayIntrv, c_uDyIntrv);
 }
 
 TEST(task_tests,
-     minute_interval_is_extracted_from_string_with_no_stars)
+     noval_days_interval_is_extracted_from_string_with_no_stars)
 {
-    const auto c_uMinIntrv = extractDaysIntrv("5 6 7 qwer");
-    ASSERT_EQ(0, c_uMinIntrv);
+    const auto c_iDaysIntrv = extractDaysIntrv("5 6 7 qwer");
+    ASSERT_EQ(gc_noval, c_iDaysIntrv);
+}
+
+TEST(task_tests,
+     noval_hours_interval_is_extracted_from_string_with_no_stars)
+{
+    const auto c_iHrIntrv = extractHoursIntrv("5 6 7 qwer");
+    ASSERT_EQ(gc_noval, c_iHrIntrv);
+}
+
+TEST(task_tests,
+     noval_minutes_interval_is_extracted_from_string_with_no_stars)
+{
+    const auto c_uMinIntrv = extractMinutesIntrv("5 6 7 qwer");
+    ASSERT_EQ(gc_noval, c_uMinIntrv);
 }
 
 TEST(task_tests,
      cmd_is_extracted_from_string)
 {
-    const auto strCmd = extractCmd(sc_strFracs);
+    const auto strCmd = extractCmd(sc_strNums);
     ASSERT_EQ(sc_strCmd, strCmd);
 }
 
